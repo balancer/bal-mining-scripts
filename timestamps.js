e9547fe1966e8f1936bfd11b552c096b99ba3b6c
Mike McDonald <mikeraymcdonald@gmail.com>
2020-06-08 14:12:50 -0500

    historical coineckgo prices, progress tracking

--- timestamps.js
+++ timestamps.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const Web3 = require('web3');
+const { argv } = require('yargs');
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider(`wss://mainnet.infura.io/ws/v3/${process.env.INFURA_ID}`));
 
 (async function() {
 
-    for (let i = 10131594; i < 10138872; i++) {
+    for (let i = argv.startBlock; i <= argv.endBlock; i++) {
         let block = await web3.eth.getBlock(i);
         console.log(`${i} - ${block.timestamp} - ${(new Date(block.timestamp * 1000)).toUTCString()}`)
     }

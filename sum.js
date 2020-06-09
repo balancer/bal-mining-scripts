1c4dd80a322de9e12cea39477e8ac302f3ab93b0
Mike McDonald <mikeraymcdonald@gmail.com>
2020-06-09 00:27:36 -0500

    week one run and cleanup

--- sum.js
+++ sum.js
@@ -1,31 +1,44 @@
 const Web3 = require('web3');
 const fs = require('fs');
 const utils = require('./utils');
+const BigNumber = require('bignumber.js');
+const { argv } = require('yargs');
 
+function bnum(val) {
+    return new BigNumber(val.toString());
+}
+
+const END_BLOCK = argv.endBlock; // Closest block to reference time at end of week
+const START_BLOCK = argv.startBlock; // Closest block to reference time at beginning of week
+const BLOCKS_PER_SNAPSHOT = 64;
 
 (async function() {
 
     const userTotals = {};
+    const userBal = {};
+
+    let balTotal = bnum(0)
     
     try {
         // Get all files in report directory
-        const files = await fs.promises.readdir('./reports');
+        for (i = END_BLOCK; i > START_BLOCK; i -= BLOCKS_PER_SNAPSHOT) {
 
-        files.forEach(file=> {
-            const jsonString = fs.readFileSync(`./reports/${file}`)
+            const jsonString = fs.readFileSync(`./reports/1/${i}.json`)
             const report = JSON.parse(jsonString)[1];
 
             Object.keys(report).forEach(user => {
+                balTotal = balTotal.plus(bnum(report[user]))
                 if (userTotals[user]) {
-                    userTotals[user] += report[user]
+                    userTotals[user] = bnum(userTotals[user]).plus(bnum(report[user])).toString()
                 } else {
-                    userTotals[user] = report[user]
+                    userTotals[user] = report[user];
                 }
             })
 
-        })
+        }
+        console.log(balTotal.toString())
+        fs.writeFileSync(`./_totals.json`, JSON.stringify(userTotals, null, 4));
 
-        console.log(JSON.stringify(userTotals))
     }
     catch( e ) {
         console.error( "Error reading reports", e );

a482009ca31d7e3d5728fb2f6d48255c5004a094
bonustrack <fabien@bonustrack.co>
2020-11-03 22:16:03 +0700

    Add script to get merkle roots

---
+++ src/getMerkleRoots.ts
@@ -0,0 +1,17 @@
+import { loadTree } from './merkle';
+import requireContext from 'require-context';
+
+const network = process.env.NETWORK || 'kovan';
+const networkStr = network === 'kovan' ? '-kovan' : '';
+const requireFile = requireContext(`../../reports${networkStr}`, true, /_totals.json$/);
+const reports = Object.fromEntries(
+    requireFile.keys()
+        .map((fileName) => [fileName.replace('/_totals.json', ''), requireFile(fileName)])
+);
+
+console.log('Merkle roots');
+Object.entries(reports).forEach(([week, report]) => {
+    const merkleTree = loadTree(report);
+    console.log(`Week ${week}`);
+    console.log(merkleTree.getHexRoot());
+});

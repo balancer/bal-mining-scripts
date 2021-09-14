31d1522af99ed94e8403440291b6da0d368f5762
Greg Taschuk <gregorytaschuk@gmail.com>
2021-09-13 21:42:04 -0700

    added config for arbitrum merkle roots script

--- js/src/config.ts
+++ js/src/config.ts
@@ -14,6 +14,14 @@ const liquidityMiningConfig = {
         fleekNamespace: 'balancer-claims-lido',
         offset: 63,
     },
+    arbitrum: {
+        reportsDirectory: `../reports/`,
+        reportFilename:
+            '/__arbitrum_0x040d1edc9569d4bab2d15287dc5a4f10f56a56b8.json',
+        jsonSnapshotFilename: '_current-arbitrum.json',
+        fleekNamespace: 'balancer-claims-arbitrum',
+        offset: 67,
+    },
     kovan: {
         reportsDirectory: `../reports-kovan/`,
         reportFilename: '_totals.json',

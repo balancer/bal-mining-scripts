94352f508e4e9b508366366d61e8a71b0b0e5467
Greg Taschuk <gregorytaschuk@gmail.com>
2021-11-11 10:35:38 -0800

    use copy-files babel option to copy json config to js/lib

--- js/src/config.ts
+++ js/src/config.ts
@@ -1,4 +1,4 @@
-import liquidityMiningConfig from './config/liquidityMiningConfig.json';
+import * as liquidityMiningConfig from './config/liquidityMiningConfig.json';
 
 const network = process.env.NETWORK || 'homestead'; // || 'kovan' || 'homestead-lido'
 const config = liquidityMiningConfig[network];
